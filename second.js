209a7a925807f14eacf867eddb5cab1b10982eac
Mohammad Zaid <91260318+zaid1398@users.noreply.github.com>
2022-01-03 13:31:25 +0530

    Create second.js

---
+++ second.js
@@ -0,0 +1,32 @@
+/**
+ * check whether the given char is vowel or not?
+ */
+
+ char = "O"
+ if(char=="a" || char=="A")
+ {
+     alert("Vowel number");
+ }
+ else if(char=="e" || char=="E")
+ {
+     alert("Vowel number");
+ }
+ else if(char=="e"|| char=="E")
+ {
+     alert("Vowel number");
+ }
+ else if(char=="i"|| char=="I")
+ {
+     alert("Vowel number");
+ }
+ else if(char=="o"||char=="O")
+ {
+     alert("Vowel number");
+ }
+ else if(char == "u" || char=="U")
+ {
+     alert("Vowel number");
+ }
+ else{
+     alert("Not a vowel number");
+ }
